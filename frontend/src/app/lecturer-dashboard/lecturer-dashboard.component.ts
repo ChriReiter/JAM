f1bc5a6f18311376ea95dbe6fb541891686f24f1
Matrot12 <matthiasroth@gmx.at>
2023-01-31 11:49:24 +0100

    #13 calendar database

--- frontend/src/app/lecturer-dashboard/lecturer-dashboard.component.ts
+++ frontend/src/app/lecturer-dashboard/lecturer-dashboard.component.ts
@@ -13,6 +13,8 @@ import { Component } from '@angular/core';
       <hr/>
       <h2>Vacancies</h2>
       <app-vacancies-list [defaultSize]="5"></app-vacancies-list>
+      <h2>Calendar</h2>
+      <app-calendar></app-calendar>
     </div>
   `,
   styleUrls: ['./lecturer-dashboard.component.scss']
